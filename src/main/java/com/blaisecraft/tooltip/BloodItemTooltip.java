655f9428f45b035e6342edbd72154fc5a3438c48
bmw-pc <blaise.wynne1@gmail.com>
2025-10-14 16:43:29 +1100

    version 0.1.7

---
+++ src/main/java/com/blaisecraft/tooltip/BloodItemTooltip.java
@@ -0,0 +1,20 @@
+import net.minecraft.component.type.TooltipDisplayComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.function.Consumer;
+
+public class BloodItemTooltip extends Item {
+    public BloodItemTooltip(Settings settings) {
+        super(settings);
+    }
+
+    @SuppressWarnings("depreciation")
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type) {
+        textConsumer.accept(Text.translatable("itemTooltip.blaisecraft.blood_item").formatted(Formatting.DARK_GRAY, Formatting.ITALIC));
+    }
+}
